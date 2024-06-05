9ede4f93ad9fb8fe5993070ed456cb358dff8fde
unknown <sashenhasindu@gmail.com>
2024-06-05 23:44:27 +1000

    Convex & Clerk

---
+++ components/auth/loading.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+
+type Props = {};
+
+function Loading({}: Props) {
+  return (
+    <div className="h-fll w-full flex-col justify-center items-center">
+      Loading
+    </div>
+  );
+}
+
+export default Loading;
